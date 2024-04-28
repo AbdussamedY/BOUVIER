afede9a4cdc4beecc10e77349402fc619b970afa
Abdussamed <122764454+AbdussamedY@users.noreply.github.com>
2024-04-28 11:37:07 +0200

    Add files via upload

--- GUI_animal_selection.py
+++ GUI_animal_selection.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
+import os
+
+current_dir = os.getcwd()
+parent_dir = os.path.dirname(current_dir)
+census_path = os.path.join(parent_dir, 'census.xlsx')
 
 def selection_change():
     global raw_data_path
@@ -18,29 +23,28 @@ def show_outputs_change():
 root = tk.Tk()
 root.title("Select one animal for rotation in pitch dark analysis")
 
-
 button_frame = ttk.Frame(root)
-button_frame.pack(side='right', padx=10, pady=10)  
+button_frame.pack(side='right', padx=10, pady=10)
 
 selected_option = tk.StringVar()
 
-
-animal_list = pd.read_excel(r'C:\Users\gbouvier\Documents\census.xlsx', sheet_name='Study')['raw_data_path']
+animal_list = pd.read_excel(census_path, sheet_name='Study')['raw_data_path']
 
 for i, animal in enumerate(animal_list):
     radio_button = tk.Radiobutton(root, text=animal, variable=selected_option, value=animal, command=selection_change)
     radio_button.pack(anchor='w')
 
-
 save_figures_var = tk.BooleanVar()
 save_figures_checkbox = ttk.Checkbutton(button_frame, text="Save figures", variable=save_figures_var, command=save_figures_change)
 save_figures_checkbox.pack(anchor='w')
-
 
 show_outputs_var = tk.BooleanVar()
 show_outputs_checkbox = ttk.Checkbutton(button_frame, text="Show outputs", variable=show_outputs_var, command=show_outputs_change)
 show_outputs_checkbox.pack(anchor='w')
 
+# Ajouter un bouton 'Close' pour fermer la fenêtre
+close_button = ttk.Button(root, text="Close", command=root.destroy)  # Méthode pour fermer la fenêtre
+close_button.pack(side='bottom', pady=10)  # Positionner le bouton en bas
 
 Saving_boolean = False
 Showing_boolean = False
